9d6bee70060f9911bac81f646b56d8becafd37d8
mahmoudra458 <mahmoodhassbat@gmail.com>
2023-06-21 10:43:11 +0300

    Added Eng.mahmoudramdan src code

---
+++ DFT&FFT_Effect/main.py
@@ -0,0 +1,15 @@
+from IPython.display import Audio
+import librosa
+import librosa.display
+from scipy.io.wavefile import write
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+Audio("signal_ftt.wav")
+#x = np.asarray(signal[1])
+#y = fft(x)
+#y
+
+
+
+sf.write('new_file.wav', y, 44100)
